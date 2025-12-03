78c2cb53d3706e5202f15e68ead84785c1af24ac
NANDANA <NANDANA@LAPTOP-7UVDSG1P>
2025-12-03 17:46:10 +0530

    This is My Second Commit

--- ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class1Test.java
+++ ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class1Test.java
@@ -8,7 +8,9 @@ public class Class1Test {
 	@Test
 	public void class1() {
  
-		Reporter.log(">>>>>EAT 5ve STAR 🍫,DO NOTHING😉😉😉<<<<<",true);
-		
+		Reporter.log(">>>>>EAT 5ve STAR 🍫,DO NOTHING<<<<<",true);
+		Reporter.log(">>>>>>>>>>>>>>>>OLD<<<<<<<<<<<<<<<<<",true);
+		Reporter.log(">>>>>EAT 5ve STAR 🍫,DO ANYTHING<<<<<",true);
+
 	}
 }
