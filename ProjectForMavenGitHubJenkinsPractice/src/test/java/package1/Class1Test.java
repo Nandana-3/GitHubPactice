bbf316032c2f4c8da7d1baaac9eb2f7185e22588
NANDANA <NANDANA@LAPTOP-7UVDSG1P>
2025-12-03 17:30:51 +0530

    My first commit

---
+++ ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1Test {
+
+	@Test
+	public void class1() {
+ 
+		Reporter.log(">>>>>EAT 5ve STAR 🍫,DO NOTHING😉😉😉<<<<<",true);
+		
+	}
+}
